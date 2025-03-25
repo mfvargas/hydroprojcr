2dc0336c233a4d9a47f2737c8f60a3e835652773
mfvargas <mfvargas@gmail.com>
2025-03-24 22:43:17 -0600

    Modificar pruebas-parquet.R - agregar instalación de extensión httpfs para acceder archivos remotos

--- pruebas-parquet.R
+++ pruebas-parquet.R
@@ -2,12 +2,25 @@ library(DBI)
 library(duckdb)
 
 
-bd <- 'parquet/data_Q_45_2.parquet'
+# Archivo Parquet local
+# bd <- 'parquet/data_Q_45_2.parquet'
+
+# Archivo Parquet remoto
+bd <- 'https://github.com/mfvargas/hydroprojcr/raw/refs/heads/main/parquet/data_Q_45_2.parquet'
+
+# Archivo CSV de salida
 csv <- 'csv/data_Q_45_2.csv'
+
+# Parámetro para filtrar los datos
 subid <- 605
+
 
 # Crear conexión a DuckDB (en memoria)
 conexion <- dbConnect(duckdb())
+
+# Instalar y cargar la extensión httpfs para acceder archivos remotos
+dbExecute(conexion, "INSTALL httpfs;")
+dbExecute(conexion, "LOAD httpfs;")
 
 # Crear consulta SQL
 consulta <- paste0(
